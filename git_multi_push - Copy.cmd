2d2baf77a79170fd3e1c104f88dfffae195307bd
drquochoai <drquochoai@gmail.com>
2015-04-03 10:14:13 +0700

    Tran Quoc Hoai ba cai xi dao

--- git_multi_push - Copy.cmd
+++ git_multi_push - Copy.cmd
@@ -1,11 +1,21 @@
+@ECHO OFF
 SET /P chaytu=[chaytu: Chay tu so may]
 SET /P chayden=[chayden: Chay den so may]
-set sohientai = %chaytu%
+echo # bsquochoai.ga Hoc nha van ba dao tai av.bsquochoai.com >> README.md
+git init
 :looplai
 
+git add README.md
+git commit -m "Tran Quoc Hoai"
+git remote rm origin
+git remote add origin https://github.com/bsquochoainew/ensg_src%sohientai%.git
+git push -u origin master
 
-if "%sohientai%" == chayden+1 goto end 
+git checkout -b bsquochoai
+git add .
+git commit -m "Tran Quoc Hoai ba cai xi dao"
+git push -u origin bsquochoai
+
 set /a "sohientai+=1"
+
 goto looplai
-:end
-pause
--- git_multi_push.cmd
+++ git_multi_push.cmd
@@ -1,6 +1,8 @@
-SET /P chaytu=[chaytu: Chay tu so may]
-SET /P chayden=[chayden: Chay den so may]
-set /a
+@echo off
+SET /P chaytu=[chaytu: FROM]
+SET /P chayden=[chayden: END]
+SET /P project=[project: project name]
+set sohientai=%chaytu%
 echo # bsquochoai.ga Hoc nha van ba dao tai av.bsquochoai.com >> README.md
 git init
 :looplai
@@ -8,7 +10,7 @@ git init
 git add README.md
 git commit -m "Tran Quoc Hoai"
 git remote rm origin
-git remote add origin https://github.com/bsquochoainew/ensg_src%sohientai%.git
+git remote add origin https://github.com/bsquochoainew/%project%%sohientai%.git
 git push -u origin master
 
 git checkout -b bsquochoai
@@ -16,6 +18,9 @@ git add .
 git commit -m "Tran Quoc Hoai ba cai xi dao"
 git push -u origin bsquochoai
 
+if "%sohientai%" == "%chayden%" goto end 
 set /a "sohientai+=1"
-
 goto looplai
+:end
+echo "XONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+pause
